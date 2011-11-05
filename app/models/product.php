a8849d06a9fe9c493a44992c9c3c9830efab8f1a
Stan Patzschke <stan.patzschke@googlemail.com>
2011-11-05 12:12:56 +0100

    data input

--- app/models/product.php
+++ app/models/product.php
@@ -31,16 +31,6 @@ class Product extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'description' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'product_number' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
